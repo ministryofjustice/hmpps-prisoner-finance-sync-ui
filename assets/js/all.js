24b3fda32a59d95460f1968b3517f74dc576dec5
jasonwroe-mt <jason.wroe@madetech.com>
2026-01-20 16:59:22 +0000

    Put all JS code initialisation into all.js
    Put all JS code initialisation into all.js

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,10 +1,18 @@
 import ListFilter from './list-filter'
 import nodeListForEach from './utils'
+import Card from './card'
 
 function initAll() {
     var $filters = document.querySelectorAll('[data-module="moj-filter"]')
-        nodeListForEach($filters, function ($filter) {
+    
+    nodeListForEach($filters, function ($filter) {
         new ListFilter($filter)
+    })
+
+    const $cards = document.querySelectorAll('.card--clickable')
+    nodeListForEach($cards, $card => {
+      // eslint-disable-next-line no-new
+      new Card($card)
     })
 }
 
