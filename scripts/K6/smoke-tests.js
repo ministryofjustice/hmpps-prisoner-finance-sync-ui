0126922bfb4fde5769f6b3a8853b9dfcac5f49f8
sam.caine <sam.caine@justice.gov.uk>
2026-09-04 10:01:08 +0100

    fix linting; ignore smoke test scripts from linting

--- scripts/K6/smoke-tests.js
+++ scripts/K6/smoke-tests.js
@@ -1,12 +1,12 @@
 import http from 'k6/http'
 import { check } from 'k6'
 
-const domain = __ENV.DOMAIN;
+const domain = __ENV.DOMAIN
 
 const httpParams = {
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  headers: {
+    'Content-Type': 'application/json',
+  },
 }
 
 const baseUrl = `https://${domain}`
@@ -14,25 +14,25 @@ const baseUrl = `https://${domain}`
 export const options = {
   thresholds: {
     checks: ['rate==1.0'],
-  }
+  },
 }
 
 export default function verify_health() {
-    const res = http.get(`${baseUrl}/health`, httpParams)
+  const res = http.get(`${baseUrl}/health`, httpParams)
 
-    check(res, {
-        'status is 200': r => r.status === 200,
-        'body is defined': r => r.body !== undefined,
+  check(res, {
+    'status is 200': r => r.status === 200,
+    'body is defined': r => r.body !== undefined,
+  })
+
+  const body = res.json()
+  check(body, {
+    'overall status is UP': b => b.status === 'UP',
+  })
+
+  for (const [componentName, info] of Object.entries(body.components)) {
+    check(info, {
+      [`Component "${componentName}" status is UP (found: "${info.status}")`]: i => i.status === 'UP',
     })
-
-    const body = res.json()
-    check(body, {
-        'overall status is UP': b => b.status === 'UP',
-    })
-
-    for (const [componentName, info] of Object.entries(body.components)) {
-        check(info, {
-            [`Component "${componentName}" status is UP (found: "${info.status}")`]: i => i.status === 'UP',
-        })
-    }
+  }
 }
